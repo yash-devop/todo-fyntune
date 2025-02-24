93cd483d7703106eead426b7e64fd6989d628688
yash-devop <kamble1234meena@gmail.com>
2025-02-24 14:25:17 +0530

    type: ModalType ( add / edit )

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -4,8 +4,11 @@ import { todoSchema } from "../schemas/schemas"
 
 export type TodoSchemaType = InferType<typeof todoSchema>;
 
+
+type ModalType = "add" | "edit";
 export type ModalProps<T> = {
     open: boolean,
     setOpen: React.Dispatch<React.SetStateAction<boolean>>
-    data?: T
+    type: ModalType,
+    data?: T,
 };
