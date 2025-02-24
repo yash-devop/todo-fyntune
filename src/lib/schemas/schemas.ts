98dbdd5c8568e60d2a46706d906cc4f2f31a1054
yash-devop <kamble1234meena@gmail.com>
2025-02-24 14:26:18 +0530

    feat: restrict previous date input

--- src/lib/schemas/schemas.ts
+++ src/lib/schemas/schemas.ts
@@ -1,7 +1,11 @@
 import {object , string} from "yup"
+import { isValidDate } from "../../utils/utils";
 
 export const todoSchema = object({
     title: string().required("Title is Required"),
     description: string().optional(),
-    dueDate: string().required("Due date is Required")
+    dueDate: string().required("Due date is Required").test((date)=>{
+        const selectedDate = new Date(date);
+        return isValidDate(selectedDate)
+    })
 });
