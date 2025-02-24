0954f13e2412f443ccb4b2093cd296190260326d
yash-devop <kamble1234meena@gmail.com>
2025-02-24 18:05:11 +0530

    add: Toaster with richColors

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { ModalProvider } from './context/ModalContext.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
+import {Toaster} from "sonner"
 const queryClient = new QueryClient({
   defaultOptions:{
       queries:{
@@ -17,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
     <ModalProvider>
       <QueryClientProvider client={queryClient}>
         <App />
+        <Toaster richColors/>
       </QueryClientProvider>
     </ModalProvider>
   </StrictMode>,
