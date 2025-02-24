34cda5f98f1b8ee7bd910cee53cacd353bac8877
yash-devop <kamble1234meena@gmail.com>
2025-02-24 15:37:18 +0530

    add: tanstack query provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ModalProvider } from './context/ModalContext.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient({
+  defaultOptions:{
+      queries:{
+          refetchOnWindowFocus: false
+      }
+  }
+})
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ModalProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ModalProvider>
   </StrictMode>,
 )
