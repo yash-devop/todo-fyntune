9b7523b7ed44ee00455b50a7b49e6745aeee82b1
yash-devop <kamble1234meena@gmail.com>
2025-02-24 14:24:47 +0530

    feat: isValidDate

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,14 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+import {isValid , isBefore , startOfDay} from "date-fns"
+
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+export function isValidDate(date:Date){
+  if(!isValid(date)) throw new Error("Date passed to isValid fn is not valid.");
+  return !isBefore(date, startOfDay(new Date()))
 }
