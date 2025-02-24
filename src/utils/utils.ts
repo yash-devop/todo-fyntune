0bcdbd6a3d49f19ab5430ff753aa4e4b91cc52e9
yash-devop <kamble1234meena@gmail.com>
2025-02-24 17:17:16 +0530

    feat: formatDueDate

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import {isValid , isBefore , startOfDay} from "date-fns"
+import {isValid , isBefore , startOfDay, parseISO, isToday, isTomorrow, format} from "date-fns"
 
 
 export function cn(...inputs: ClassValue[]) {
@@ -12,3 +12,10 @@ export function isValidDate(date:Date){
   if(!isValid(date)) throw new Error("Date passed to isValid fn is not valid.");
   return !isBefore(date, startOfDay(new Date()))
 }
+
+export const formatDueDate = (dateStr: string) => {
+  const date = parseISO(dateStr);
+  if (isToday(date)) return "Due Today";
+  if (isTomorrow(date)) return "Due Tomorrow";
+  return format(date, "MMM dd, yyyy");
+};
