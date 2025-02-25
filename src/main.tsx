51fd43d1a750ae1e3c54c3eb1adb91ab3f6ec2b5
yash-devop <kamble1234meena@gmail.com>
2025-02-25 17:17:33 +0530

    add: color switcher

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { ModalProvider } from './context/ModalContext.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import {Toaster} from "sonner"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ModalProvider } from "./context/ModalContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+import { BrowserRouter as Router } from "react-router";
+import ColorSwitcher from "./components/ColorSwitcher.tsx";
 const queryClient = new QueryClient({
-  defaultOptions:{
-      queries:{
-          refetchOnWindowFocus: false
-      }
-  }
-})
-createRoot(document.getElementById('root')!).render(
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ModalProvider>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <Toaster richColors/>
-      </QueryClientProvider>
-    </ModalProvider>
-  </StrictMode>,
-)
+    <Router>
+      <ModalProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <Toaster richColors />
+          <ColorSwitcher />
+        </QueryClientProvider>
+      </ModalProvider>
+    </Router>
+  </StrictMode>
+);
