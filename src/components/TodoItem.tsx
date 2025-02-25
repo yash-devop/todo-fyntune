2154bfe9cf9e816fec30b2b739318b7c9bbf614f
yash-devop <kamble1234meena@gmail.com>
2025-02-25 17:17:03 +0530

    component: todo item

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -8,63 +8,94 @@ import { DueBadge } from "./DueBadge";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { fetcher } from "../lib/fetcher";
 import { toast } from "sonner";
+import StrikeThrough from "./Strikethrough";
 
-export default function TodoItem({
-    todos
-}: {
-    todos: ExtendedTodoSchemaType
-}){
-    const [open , setOpen] = useState<boolean>(false);
+export default function TodoItem({ todos }: { todos: ExtendedTodoSchemaType }) {
+  const [open, setOpen] = useState<boolean>(false);
+  const [isDone, setIsDone] = useState<boolean>(todos.status === "completed");
 
-    const {mutate} = useMutation({
-        mutationFn: fetcher
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: fetcher,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+    onError: () => {
+      toast.error("Something went wrong.");
+    },
+  });
+
+  const handleDelete = () => {
+    mutate({
+      method: "DELETE",
+      path: `/todos/${todos.id}`,
+      body: "",
     });
-    const queryClient = useQueryClient()
 
-    const handleDelete=()=>{
-        mutate({
-            method: "DELETE",
-            path: `/todos/${todos.id}`,
-            body: ""
-        }, {
-            onSuccess: ()=>{
-                queryClient.invalidateQueries({
-                    queryKey: ["todos"]
-                })
-                toast.success("Todo Deleted Successfully")
-                setOpen(false);
-            },
-            onError: ()=>{
-                toast.success("Something went wrong.");
-                setOpen(false);
-            }
-        })
-    }
-    return (
-        <>
-            <div className={`flex justify-between p-3 w-full group border border-border rounded-xl`}>
-                <div className="flex items-start gap-3 w-full">
-                    <Checkbox className="shrink-0 group size-5 rounded-full border bg-white data-[checked]:bg-white flex items-center justify-center cursor-pointer">
-                        <CheckIcon size={17} className="hidden group-data-[checked]:block"/>
-                    </Checkbox>
-                    <div className="flex items-start justify-between w-full">
-                        <div className="flex flex-col">
-                            <h1 className="text-lg font-medium">{todos?.title}</h1>
-                            <p className="max-w-[550px] truncate text-neutral-500">{todos.description}</p>
-                            <DueBadge data={todos.dueDate}/>
-                        </div>
-                        <div className="hidden gap items-center group-hover:flex">
-                            <Button onClick={()=>setOpen(true)} size={"sm"} variant={"link"}>
-                                <PencilLineIcon size={19} className="cursor-pointer"/>
-                            </Button>
-                            <Button onClick={()=>handleDelete()} size={"sm"} variant={"link"}>
-                                <Trash2 size={19} className="cursor-pointer text-destructive"/>
-                            </Button>
-                        </div>
-                    </div>
-                </div>
-                <Modal type="edit" open={open} setOpen={setOpen} data={todos} />
+    toast.success("Todo Deleted Successfully");
+    setOpen(false);
+  };
+
+  const handleToggleStatus = () => {
+    const newStatus = isDone ? "pending" : "completed";
+
+    mutate({
+      method: "PATCH",
+      path: `/todos/${todos.id}`,
+      body: JSON.stringify({ status: newStatus }),
+    });
+
+    setIsDone(!isDone);
+  };
+
+  console.log("Each Todo item", todos);
+  return (
+    <>
+      <div
+        className={`flex justify-between p-3 w-full group border border-border rounded-xl ${
+          isDone &&
+          "bg-neutral-300 text-neutral-500 opacity-50 select-none cursor-not-allowed"
+        } transition-all `}
+      >
+        <div className="flex items-start gap-3 w-full">
+          <Checkbox
+            checked={isDone}
+            onClick={handleToggleStatus}
+            className="mt-1 shrink-0 transition-all group size-5 rounded-md border data-[checked]:border-primary bg-white data-[checked]:bg-primary/20 flex items-center justify-center cursor-pointer"
+          >
+            {isDone && <CheckIcon size={17} className="text-primary" />}
+          </Checkbox>
+
+          <div className="flex items-start justify-between w-full">
+            <div className="flex flex-col">
+              {isDone ? (
+                <StrikeThrough text={todos.title} />
+              ) : (
+                <h1 className="text-lg font-medium">{todos?.title}</h1>
+              )}
+              <p className="max-w-[550px] truncate text-neutral-500">
+                {todos.description}
+              </p>
+              <DueBadge data={todos.dueDate} isDone={isDone} />
             </div>
-        </>
-    )
+
+            <div
+              className={`hidden gap items-center ${
+                !isDone ? "group-hover:flex" : ""
+              }`}
+            >
+              <Button onClick={() => setOpen(true)} size="sm" variant="link">
+                <PencilLineIcon size={19} className="cursor-pointer" />
+              </Button>
+              <Button onClick={handleDelete} size="sm" variant="link">
+                <Trash2 size={19} className="cursor-pointer text-destructive" />
+              </Button>
+            </div>
+          </div>
+        </div>
+        <Modal type="edit" open={open} setOpen={setOpen} data={todos} />
+      </div>
+    </>
+  );
 }
